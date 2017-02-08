70aaa985050e56d2f6315b874d8a9b8f735bbac6
Michela Marchini <mmarchini@stuy.edu>
2017-02-07 23:17:35 -0500

    added 01Recursion folder

---
+++ 01Recursion/Recursion.java
@@ -0,0 +1,35 @@
+public class Recursion{
+
+    public static String name(){
+	return "Marchini,Michela";
+    }
+    
+    public static double sqrt(double n){
+	if (n < 0){
+	    throw new IllegalArgumentException();
+	}
+	else{
+	    return sqrtGuess(n, 1);
+	}
+    }
+
+    public static double sqrtGuess(double n, double guess){
+	if (closeEnough(guess*guess, n)){
+	    return guess;
+	}
+	else{
+	    return sqrtGuess(n, ( n / guess + guess) / 2);
+	}
+    }
+
+    public static boolean closeEnough (double n1, double n2){
+	double diff = n1 - n2;
+	return (diff / n1) <= .0000000000000001 && (diff / n1) >= -.0000000000000001;
+    }
+
+    public static void main(String[]args){
+	System.out.println(sqrt(121));
+    }
+		       
+
+}
