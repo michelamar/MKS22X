e25ef59be5eabe4a8ccc75083ff395969b7d411d
michelamar <mmarchini@stuy.edu>
2017-02-16 15:32:38 -0500

    compiles, but doesn't work. Try testing spot generator code to make sure it works

--- 03KnightTour/KnightBoard.java
+++ 03KnightTour/KnightBoard.java
@@ -12,49 +12,82 @@ public class KnightBoard{
 	}
     }
 
+    public void solve(){
+	solveH(0, 0, 1);
+
+    }
+
     private boolean solveH(int row, int col, int number){
 	if (number == board.length * board[0].length){ 
 	    return true;
 	}
 	else{
-	    for (	
+	    for (Integer[] coordinate : spotGenerator(row, col)){
+		board[coordinate[0]][coordinate[1]] = number;
+		if (solveH(coordinate[0], coordinate[1], number+1)){
+		    return true;
+		}
+		else{
+		    board[coordinate[0]][coordinate[1]] = 0;	
+		}
+	    }
+	    return false;
 	}
-
     }
 
     private ArrayList<Integer[]> spotGenerator (int row, int col){
 	ArrayList<Integer[]> spots = new ArrayList<Integer[]>();
 	if (row + 2 < board.length && col + 1 < board[0].length &&
 		 board[row+2][col+1] == 0){
-	    spots.add([row+2, col+1]);
+	    spots.add(new Integer[]{row+2, col+1});
 	}
-	if (row + 2 < board.length && col - 1 < board[0].length &&
+	if (row + 2 < board.length && col - 1 > 0 &&
 		 board[row+2][col-1] == 0){
-	    spots.add([row+2, col-1]);
+	    spots.add(new Integer[]{row+2, col-1});
 	}
-	if (row - 2 < board.length && col + 1 < board[0].length &&
+	if (row - 2 > 0 && col + 1 < board[0].length &&
 		 board[row-2][col+1] == 0){
-	    spots.add([row-2, col+1]);
+	    spots.add(new Integer[]{row-2, col+1});
 	}
-	if (row - 2 < board.length && col - 1 < board[0].length &&
+	if (row - 2 > 0 && col - 1 > 0 &&
 		 board[row-2][col-1] == 0){
-	    spots.add([row-2, col-1]);
+	    spots.add(new Integer[]{row-2, col-1});
 	}
 	if (row + 1 < board.length && col + 2 < board[0].length &&
 		 board[row+1][col+2] == 0){
-	    spots.add([row+1, col+2]);
+	    spots.add(new Integer[]{row+1, col+2});
 	}
-	if (row - 1 < board.length && col + 2 < board[0].length &&
+	if (row - 1 > 0 && col + 2 < board[0].length &&
 		 board[row-1][col+2] == 0){
-	    spots.add([row-1, col+2]);
+	    spots.add(new Integer[]{row-1, col+2});
 	}
-	if (row + 1 < board.length && col - 2 < board[0].length &&
+	if (row + 1 < board.length && col - 2 > 0 &&
 		 board[row+1][col-2] == 0){
-	    spots.add([row+1, col-2]);
+	    spots.add(new Integer[]{row+1, col-2});
 	}
-	if (row - 1 < board.length && col - 2 < board[0].length &&
+	if (row - 1 > 0 && col - 2 > 0 &&
 		 board[row-1][col-2] == 0){
-	    spots.add([row-1, col-2]);
+	    spots.add(new Integer[]{row-1, col-2});
 	}
 	return spots;
+    }
+
+    public String toString(){
+    	String toPrint = "" ;
+	for (int row = 0; row < board.length; row++){
+	    for (int col = 0; col < board[row].length; col++){
+		toPrint+=board[row][col] + "   ";
+	    }
+	    toPrint += "\n";
+	}
+	return toPrint;
+    }
+
+    
+
+    public static void main(String[]args){
+	KnightBoard a = new KnightBoard(7, 7);
+	a.solve();
+	System.out.println(a);
+    }
 }
