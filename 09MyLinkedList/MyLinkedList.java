23bbfbfa24ba4ad093dbc47e6a004fd14d76aa11
michelamar <mmarchini@stuy.edu>
2017-04-05 15:33:59 -0400

    add worked when it was adding to start but doesn't now :(

--- 09MyLinkedList/MyLinkedList.java
+++ 09MyLinkedList/MyLinkedList.java
@@ -12,23 +12,51 @@ public class MyLinkedList{
     public boolean add (int value){
 	LNode toAdd = new LNode(value);
 	if(size == 0){
-	    start = toAdd;
+	    end = toAdd;
+	    toAdd.setPrev(start);
 	}
-	toAdd.setNext(start);
-	toAdd.setPrev(null);
-	start.setPrev(toAdd);
-	//System.out.println (toAdd);
-	start = toAdd;
+	else{
+	    toAdd.setPrev(end);
+	    toAdd.setNext(null);
+	    end.setNext(toAdd);
+	    //System.out.println (toAdd);
+	    end = toAdd;
+	}
 	size++;
 	return true;
     }
 
+    public int get (int index){
+	LNode n = getNthNode(index);
+	return n.value;
+    }
+
+    public int set (int index, int value){
+	LNode n = getNthNode(index);
+	int oldVal = n.value;
+	n.value = value;
+	return oldVal;
+    }
+
+    private LNode getNthNode(int n){
+	LNode current = start;
+	for (int pos = 1; pos < n; pos++){
+	    current = current.next;
+	}
+	return current;
+    }
+
     public String toString(){
-	String ans = "";
+	String ans = "[";
 	LNode current = start;
 	for (int pos = 0; pos < size; pos++){
-	    ans += current.value + ", ";
-	    current = current.next;
+	    if (pos != size-1){
+	    	ans += current.value + ", ";
+	    	current = current.next;
+	    }
+	    else{
+	 	ans += current.value + "]";
+	    }
 	}
 	return ans;
     }
@@ -61,6 +89,8 @@ public class MyLinkedList{
     public static void main(String[]args){
 	MyLinkedList a = new MyLinkedList();
 	a.add(8);
+	a.add(4);
+	a.add(7);
 	System.out.println(a);
     }
 
