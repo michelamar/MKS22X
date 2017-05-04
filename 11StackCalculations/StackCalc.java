6f01d00b8313c80aaa6287e073f93a9905fdbf4e
Michela <michela@John-Marchini.local>
2017-05-03 22:21:18 -0400

    Stack calc tested and working

--- 11StackCalculations/StackCalc.java
+++ 11StackCalculations/StackCalc.java
@@ -1,25 +1,27 @@
+import java.util.*;
+
 public class StackCalc{
 
-    public static int eval (String s){
-	tokens = s.split(" ");
-	Stack<Double> values = new Stack<Double>;
-	for (token:tokens){
+    public static double eval (String s){
+	String[] tokens = s.split(" ");
+	Stack<Double> values = new Stack<Double>();
+	for (String token:tokens){
 	    if (isOperator(token)){
-		a = apply(token, values.pop(), values.pop());
-		a.push(values);
+		double a = apply(token, values.pop(), values.pop());
+		values.push(a);
 	    }
 	    else{
-		push token;
+		values.push(Double.parseDouble(token));
 	    }
 	}
 	return values.pop();
     }
 
-    public boolean isOperator(String a){
+    public static boolean isOperator(String a){
 	return a.equals("+") || a.equals("-") || a.equals("/") || a.equals("*");
     }
 
-    public static double (String op, double a, double b){
+    public static double apply(String op, double a, double b){
 	if (op.equals("+")){
 	    return a + b;
 	}
@@ -33,6 +35,16 @@ public class StackCalc{
 	    return b / a;
 	}
     }
+
+    public static void main(String[] args){
+        System.out.println(StackCalc.eval("10 2 +"));//12.0
+        System.out.println(StackCalc.eval("10 2 -"));//8.0
+        System.out.println(StackCalc.eval("10 2.0 +"));//12.0
+        System.out.println(StackCalc.eval("11 3 - 4 + 2.5 *"));//30.0
+    	System.out.println(StackCalc.eval("8 2 + 99 9 - * 2 + 9 -"));//893.0
+   	 System.out.println(StackCalc.eval("10 2 + 10 * 1 + 1 1 1 + + + 10 10 + -"));//104.0
+    }
+
  
 
 }
