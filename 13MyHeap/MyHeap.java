ea323de95ad21d50f36d6976fcfea7ec78b55140
michelamar <mmarchini@stuy.edu>
2017-05-04 15:33:08 -0400

    MyHeap written but not tested

--- 13MyHeap/MyHeap.java
+++ 13MyHeap/MyHeap.java
@@ -1,8 +1,10 @@
+import java.util.NoSuchElementException;
+
 public class MyHeap{
 
     private int size;
     private int type;
-    private int[] data;
+    private String[] data;
 
     public MyHeap(){
 	size = 0;
@@ -19,6 +21,61 @@ public class MyHeap{
 	    type = -1;
 	}
     }
+
+    public void add(String s){
+	if(size == data.length){
+	    String[] temp = new String[data.length * 2];
+	    for(int pos = 0; pos < data.length; pos++){
+		temp[pos] = data[pos];
+	    }
+	    data = temp;
+	}
+	data[size + 1] = s;
+	size++;
+	pushUp();
+    }
+
+    public String remove(){
+	if(size == 0){
+	    throw new NoSuchElementException();
+	}
+	else{
+	    String toReturn = data[1];
+	    data[1] = data[size];
+	    size--;
+	    pushDown();
+	    return toReturn;
+	}
+    }
+
+    public String peek(){
+	if(size == 0){
+	    throw new NoSuchElementException();
+	}
+	else{
+	    return data[1];
+	}
+    }
+
+    private void pushUp(){
+	int pos = size;
+	while(pos > 1 && type * (data[pos].compareTo(data[pos/2])) > 0){
+	    String temp = data[pos];
+	    data[pos] = data[pos/2];
+	    data[pos/2] = temp;
+	}
+    }
+
+    private void pushDown(){
+	int pos = 1;
+	while(pos < size && type * (data[pos].compareTo(data[pos*2])) < 0){
+	    String temp = data[pos];
+	    data[pos] = data[pos*2];
+	    data[pos*2] = temp;
+	}
+    }
+
+
 
 
 }
